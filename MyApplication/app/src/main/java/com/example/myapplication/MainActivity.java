762090f6a4ee73ba8fa64deb57d47a6f6684fb9e
zsh <13340634688@163.com>
2020-05-18 14:34:56 +0800

    first

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView =this.findViewById(R.id.textView);
+        textView.setText("更改数据");
+    }
+}
